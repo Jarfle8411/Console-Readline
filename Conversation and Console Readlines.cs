3cfaafa5158bebba48b8a7935ec60cae022d0d06
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 15:55:24 -0500

    Add files via upload
    Simple console readline method conversation with an output of numbers.

---
+++ Conversation and Console Readlines.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conversation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string talk1,
+                talk2 = "That's good to hear";
+
+            Console.Write("What is your name?\n");
+            talk1 = Console.ReadLine();
+
+            Console.WriteLine("How are you feeling {0}?",talk1);
+
+            Console.WriteLine("I'm okay said {0}.",talk1);
+
+            Console.WriteLine("Well {0}, that's good to hear!", talk1,talk2);
+
+            double num, num1, add;
+
+            Console.WriteLine("Will you please enter your first number?");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Now please enter your second number?");
+           num1 = Convert.ToDouble(Console.ReadLine());
+
+           add = num + num1;
+           Console.WriteLine(add);
+        }
+    }
+}
